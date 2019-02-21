e2549125dea37363296789cba4a753f4676d2483
Nicklas Holmberg <45942181+nishog04@users.noreply.github.com>
2019-02-21 22:51:21 +0100

    Allt fungerar, men uträkningarna blir ännu inte rätt

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         //  och operatorer var för sig.
 
         ArrayList<String> a = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(userInput, "+*/-()", true);
+        StringTokenizer st = new StringTokenizer(userInput, "+*/-", true);
 
         while(st.hasMoreTokens()){
             a.add(st.nextToken());
@@ -26,16 +26,44 @@ public class Main {
 
         // Vilken typ av operator är index 1 (alltså token 2)?
 
-        if (a.get(1).contains("+")){
-            calc.add(a.get(0),a.get(2));
-            a.remove(0);            // Raderar index[0] tre gånger
-            a.remove(0);
-            a.remove(0);
-            a.add(0,calc.res);     // Lägger till uträkningsresultatet som ny index[0]
+        while (a.size() > 1){
+            if (a.get(1).contains("+")){
+                calc.add(a.get(0),a.get(2));
+                a.remove(0);            // Raderar index[0] tre gånger
+                a.remove(0);
+                a.remove(0);
+                a.add(0,calc.res);     // Lägger till uträkningsresultatet som ny index[0]
+            }
+            else if (a.get(1).contains("-")){
+                calc.sub(a.get(0),a.get(2));
+                a.remove(0);
+                a.remove(0);
+                a.remove(0);
+                a.add(0,calc.res);
+            }
+            else if (a.get(1).contains("*")){
+                calc.mul(a.get(0),a.get(2));
+                a.remove(0);
+                a.remove(0);
+                a.remove(0);
+                a.add(0,calc.res);
+            }
+            else if (a.get(1).contains("/")){
+                calc.div(a.get(0),a.get(2));
+                a.remove(0);
+                a.remove(0);
+                a.remove(0);
+                a.add(0,calc.res);
+            }
+            else {
+                System.out.println("Fel");
+            }
+
         }
 
 
-        System.out.println(calc.res);
+
+        System.out.println("Svaret på " + userInput + " är: " + calc.res);
         System.out.println(a);
 
 
